d1249abbb1cfd280a3bea21179c442752b1e754d
Tuandev18 <phamtuan180704@gmail.com>
2025-08-05 17:01:14 +0700

    Component Todolist

---
+++ src/components/TaskList/TaskList.tsx
@@ -0,0 +1,34 @@
+import styles from './taskList.module.scss'
+
+interface taskListDone  {
+  doneTaskList?: boolean
+}
+
+export default function TaskList(props : taskListDone) {
+  const {doneTaskList} = props
+  return (
+    <div>
+      <h2 className={styles.title}>{doneTaskList ? 'Hoàn Thành' : 'Chưa Hoàn Thành'}</h2>
+      <div className={styles.tasks}>
+        <div className={styles.task}>
+          <input type="checkbox" className={styles.taskCheckbox}/>
+          <span className={`${styles.taskName}`}>Học Bài</span>
+          <div className={styles.taskActions}>
+            <button className={styles.taskBtn}>🖋️</button>
+            <button className={styles.taskBtn}>🗑️</button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.tasks}>
+        <div className={styles.task}>
+          <input type="checkbox" className={styles.taskCheckbox}/>
+          <span className={`${styles.taskName} ${styles.taskNameDone}`}>Học Bài</span>
+          <div className={styles.taskActions}>
+            <button className={styles.taskBtn}>🖋️</button>
+            <button className={styles.taskBtn}>🗑️</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
