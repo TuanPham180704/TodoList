92dbcb065efd1f2732941841b5349ea9eae70fd1
Tuandev18 <phamtuan180704@gmail.com>
2025-08-05 22:33:31 +0700

    LocalStorage

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,20 +1,40 @@
 import { Todo } from '../../@types/todo.type'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import TaskInput from '../TaskInput'
 import TaskList from '../TaskList'
 import styles from './todoList.module.scss'
+
+interface HandleNewTodos {
+  (todos: Todo[]): Todo[]
+}
+
+const syncReactLotal = (handleNewTodos: HandleNewTodos) => {
+  const todosString = localStorage.getItem('todos')
+  const todosObj: Todo[] = JSON.parse(todosString || '[]')
+  const newtodosObjs = [...todosObj, handleNewTodos]
+  localStorage.setItem('todos', JSON.stringify(newtodosObjs))
+}
 export default function TodoList() {
   const [todos, setTodos] = useState<Todo[]>([])
   const [currentTodo, setCurrentTodo] = useState<Todo | null>(null)
   const doneTodoList = todos.filter((todo) => todo.done)
   const notDoneTask = todos.filter((todo) => !todo.done)
+  useEffect(() => {
+    const todosString = localStorage.getItem('todos')
+    const todosObj: Todo[] = JSON.parse(todosString || '[]')
+    setTodos(todosObj)
+  })
   const addTodo = (name: string) => {
+    const handler = (todosObj: Todo[]) => {
+      return [...todosObj, todo]
+    }
     const todo: Todo = {
       name,
       done: false,
       id: new Date().toISOString()
     }
     setTodos((prev) => [...prev, todo])
+    syncReactLotal(handler)
   }
   const handleDoneTodo = (id: string, done: boolean) => {
     setTodos((prev) => {
@@ -41,14 +61,17 @@ export default function TodoList() {
     })
   }
   const finishEditTodo = () => {
-    setTodos((prev) => {
-      return prev.map((todo) => {
-        if (todo.id == (currentTodo as Todo).id) {
+    const handler = (todObj: Todo[]) => {
+      return todObj.map((todo) => {
+        if (todo.id === (currentTodo as Todo).id) {
           return currentTodo as Todo
         }
         return todo
       })
-    })
+    }
+    setTodos(handler)
+    setCurrentTodo(null)
+    syncReactLotal(handler)
   }
   return (
     <div className={styles.todoList}>
