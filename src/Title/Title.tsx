36f33fcc5fa27f094ea2d1066ef77109e2430c51
Tuandev18 <phamtuan180704@gmail.com>
2025-08-07 10:49:42 +0700

    Title useRef

--- src/Title/Title.tsx
+++ src/Title/Title.tsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from './title.module.scss'
 
 type TitleProps = {
-  address : {
-    street : string
+  address: {
+    street: string
   }
 }
 
 function Title() {
+  const h1Ref = useRef<HTMLHeadingElement>(null)
+  const clickRef = () => {
+    if (h1Ref.current) {
+      h1Ref.current.style.color = 'blue'
+    }
+  }
   return (
-    <h2 className={styles.title}>ToDoList TuanDanny🏠🪴</h2>
+    <h2 className={styles.title} ref={h1Ref} onClick={clickRef}>
+      ToDoList TuanDanny🏠🪴
+    </h2>
   )
 }
-function equal(prevState : TitleProps,newState : TitleProps) {
+function equal(prevState: TitleProps, newState: TitleProps) {
   return prevState.address.street === newState.address.street
 }
 
-
-export default React.memo(Title,equal)
+export default React.memo(Title, equal)
